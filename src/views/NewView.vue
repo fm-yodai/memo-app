7ea17ee8edcc41d65d22562b7b21c4dc8d81fca0
fm-yodai <yodai@freemind.co.jp>
2022-12-13 11:24:28 +0900

    スタイル編集

--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -8,3 +8,22 @@
   </div>
 </template>
 
+<style scoped>
+div {
+  margin-bottom: 10px;
+}
+input[type=text] {
+  width: 100%;
+}
+textarea {
+  width: 100%;
+  height: 30em;
+}
+button {
+  width: 5em;
+  margin: 3px;
+}
+.center {
+  text-align: center;
+}
+</style>
