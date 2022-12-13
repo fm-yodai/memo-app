57d071a6ea06b895135fff9aba43164ee2dc7e8e
fm-yodai <yodai@freemind.co.jp>
2022-12-13 12:09:12 +0900

    id挿入

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0,
     memos: []
   },
   getters: {
   },
   mutations: {
     save (state, newMemo) {
+      newMemo.id = ++this.state.count
       state.memos.unshift(newMemo)
     }
   },
