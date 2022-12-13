ae0acb463ed9227c498d49226d4b41d2eb1a9934
fm-yodai <yodai@freemind.co.jp>
2022-12-13 11:23:58 +0900

    view作成

---
+++ src/views/NewView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div><input type="text"></div>
+    <div><textarea></textarea></div>
+    <div class="center">
+      <button>save</button>
+    </div>
+  </div>
+</template>
+
